e14e81f3b6bb76e69e53f37c2c6e25b49d18fc3f
Xi Wu <youngjoycewu@gmail.com>
2025-11-05 12:03:41 +0800

    addon name attribute for moon

--- app/models/moon.py
+++ app/models/moon.py
@@ -5,7 +5,7 @@ from sqlalchemy import ForeignKey
 from typing import Optional
 
 class Moon(db.Model): #child
-   
+    name: Mapped[str]
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     description: Mapped[str]
     size: Mapped[str]
@@ -16,6 +16,7 @@ class Moon(db.Model): #child
     def to_dict(self):
         moon_as_dict = {
             "id": self.id,
+            "name": self.name,
             "size": self.size,
             "description":self.description,
             "planet":self.planet.name if self.planet_id else None
@@ -25,7 +26,9 @@ class Moon(db.Model): #child
     
     @classmethod
     def from_dict(cls, moon_data):
-        new_moon = cls(size=moon_data["size"],
+        new_moon = cls(
+                       name=moon_data["name"],
+                       size=moon_data["size"],
                        description=moon_data["description"],
                        planet_id=moon_data.get("planet_id", None)
                        )
